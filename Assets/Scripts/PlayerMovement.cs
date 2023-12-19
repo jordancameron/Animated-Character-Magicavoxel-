3b1d264705c81f3ca97ab76b53cd6e39a2f15188
Jordan Cameron <jordanalexandra@hotmail.com>
2023-12-19 14:37:19 -0400

    Fixed player movement
    Player movement is based on direction camera is facing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,45 @@ public class PlayerMovement : BaseMovement
     private AnimatorController myAnim;
     
     private Vector3 tempMovement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Update()
+    {
+        //tempMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        tempMovement = Input.GetAxis("Horizontal") * Camera.main.transform.right + Input.GetAxis("Vertical") * Camera.main.transform.forward;
+        tempMovement.y = 0f;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        tempMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        PlayerMove();
+        ChangeAnimation();
+    }
+
+    void PlayerMove()
+    {
+        //tempMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        //float rot = Mathf.Atan2(tempMovement.z, tempMovement.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, rot, 0f);
+
         Move(tempMovement);
+    }
+
+    void ChangeAnimation()
+    {
         if (myAnim)
         {
-            if(tempMovement.magnitude > 0f)
+            if (tempMovement.magnitude > 0f)
             {
                 myAnim.ChangeAnimBoolValue("Running", true);
+                float rot = Mathf.Atan2(-tempMovement.z, tempMovement.x) * Mathf.Rad2Deg + 90f;
+                transform.rotation = Quaternion.Euler(0f, rot, 0f);
             }
             else
             {
