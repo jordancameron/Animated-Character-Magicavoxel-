f0975c4bd54bd7ba8e6609f072ac7daa49e0f9cc
Jordan Cameron <jordanalexandra@hotmail.com>
2023-12-19 11:26:41 -0400

    Initial commit

---
+++ Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+
+public class BaseMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float rotateSpeed;
+
+    [Header("References")]
+    [SerializeField]
+    private Rigidbody myRigidbody;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(!myRigidbody)
+        {
+            myRigidbody = GetComponent<Rigidbody>();
+        }
+    }
+
+    public void Move(Vector3 moveDirection)
+    {
+        myRigidbody.velocity = moveDirection.normalized * moveSpeed;
+    }
+}
